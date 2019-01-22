65e2d25f2701bd24a7bce83ec95048d9e3f44076
dror <sametcoh@gmail.com>
2019-01-22 11:40:09 +0200

    Initial github with what i've done so far

---
+++ TowerDefense/Assets/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+        target = Waypoints.points[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // By substracting the x,y,z of our target with where we are, we get a vector of direction we need to go
+        Vector3 direction = target.position - transform.position;
+        // We normalize the vector so the only thing that controls our speed is our speed variable
+        // We multiply by deltaTime to make sure the speed isnt depandant on frame rate alone (Time.deltaTime = time passed since last frame)
+        transform.Translate(direction.normalized * speed *Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
+	}
+
+    private void GetNextWaypoint()
+    {
+        if(wavepointIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+        } else
+        {
+            wavepointIndex++;
+            target = Waypoints.points[wavepointIndex];
+        }
+    }
+}
---
+++ TowerDefense/Assets/WaveSpawner.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countDown = 2f;
+
+    public Text waveCountdownText;
+ 
+    private int waveIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(countDown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countDown = timeBetweenWaves;
+        }
+
+        waveCountdownText.text = Mathf.Ceil(countDown).ToString();
+
+        countDown -= Time.deltaTime;
+	}
+
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+        Debug.Log("Wave " + waveIndex + " Incoming!");
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
