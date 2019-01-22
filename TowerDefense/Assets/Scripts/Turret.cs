e8000bc4143c9a88cbf7403b5d0230d0783045d6
dror <sametcoh@gmail.com>
2019-01-23 00:50:15 +0200

    turret, bullets, effects and more!

---
+++ TowerDefense/Assets/Scripts/Turret.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+    private Transform target;
+
+    [Header("Attributes")]
+    public float range = 15f;
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+    public float turretSpeed = 10f;
+
+
+    [Header("Unity Setup Fields")]
+
+    public string enemyTag = "Enemy";
+
+    public Transform partToRotate;
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (target == null)
+            return;
+
+        // Target Lock On
+        Vector3 direction = target.position - transform.position;
+        // a mathematical way to deal with rotation
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        // change the rotation to what unity works with (x,y,z) rotation
+        //Vector3 rotation = lookRotation.eulerAngles;
+        // lerp is used to smooth the rotation while changing directions
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turretSpeed).eulerAngles;
+        // rotate our turret only on the y
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+
+        if(fireCountdown <= 0f)
+        {
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+	}
+
+    private void Shoot()
+    {
+        GameObject bulletGo = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGo.GetComponent<Bullet>();
+
+        if(bullet != null)
+        {
+            bullet.Seek(target);
+        }
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if(distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if(nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        } else
+        {
+            target = null;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
