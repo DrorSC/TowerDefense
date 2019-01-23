1556f5c0d3f80b43a6308f3805a018b7847da144
dror <sametcoh@gmail.com>
2019-01-24 01:53:04 +0200

    gotta sleep

--- TowerDefense/Assets/Scripts/CameraController.cs
+++ TowerDefense/Assets/Scripts/CameraController.cs
@@ -7,6 +7,10 @@ public class CameraController : MonoBehaviour {
     public float panSpeed = 30f;
     public float panBorderThinkness = 10f;
 
+    public float scrollSpeed = 5f;
+    public float minY = 10f;
+    public float maxY = 100f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -14,6 +18,9 @@ public class CameraController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if (Input.mousePosition.x < 0 || Input.mousePosition.x > Screen.width || Input.mousePosition.y < 0 || Input.mousePosition.y > Screen.height)
+            return;
+        
         if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThinkness)
         {
             transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
@@ -30,5 +37,14 @@ public class CameraController : MonoBehaviour {
         {
             transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
         }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        Vector3 camPosition = transform.position;
+
+        camPosition.y -= scroll * 1000 * scrollSpeed * Time.deltaTime;
+        camPosition.y = Mathf.Clamp(camPosition.y, minY, maxY);
+
+        transform.position = camPosition;
     }
 }
